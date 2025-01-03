20f618938ae5d38d861bf5ebff985f69576b7e74
mubbe127 <mubarek.tajudin@gmail.com>
2025-01-03 14:46:59 +0100

    Refactor get messages to one component and add display logic for messages

---
+++ src/components/Messages.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState, useMemo, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../utils/useAuth";
+import formatDate from "../utils/utils";
+
+import styles from "./Messages.module.css";
+
+function Messages({ chat = { members: [], messages: [] } }) {
+
+
+  const { authState } = useAuth();
+  const { chatId } = useParams();
+
+  const memberMap = useMemo(() => {
+    const map = {};
+    chat.members.forEach((member) => (map[member.id] = member));
+    console.log(map);
+    return map;
+  }, [chat.members]);
+  let lastMessage = {time:0, user: null};  
+  return (
+    <div className={styles.messagesContainer}>
+      {chat &&
+        chat.messages.map((message, index) => {
+      
+          const messageDate = new Date(message.createdAt).getTime();
+          const messageUser = message.userId
+          let postDate = false;
+          let postUser=false
+
+          if (
+            index === 0 ||
+            messageDate >
+              lastMessage.time + 1000 * 60 * 30
+          ) {
+            lastMessage.time=messageDate
+            postDate = true;    
+            postUser=true
+          }
+
+          if(index===0 || messageUser!==lastMessage.user){
+            lastMessage.user=messageUser
+            postUser=true
+          }
+
+          return (
+            <div className={ message.userId===authState?.user?.id ? `${styles.creator} ${styles.messageContainer}` : styles.messageContainer} key={message.id}>
+              {/* Displaying formatted date */}
+              {postDate && <div  className={styles.date}><p>{formatDate(message.createdAt)}</p></div>}
+              {postUser && <div className={styles.user}><p >{(memberMap[message.userId]?.firstname || "Unknown User")}</p></div>}
+              <div className={styles.content}><p >{message.content}</p></div>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default Messages;
