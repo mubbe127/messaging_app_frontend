b55680a130582d5247c152021a465a0a8a98bdfb
mubbe127 <mubarek.tajudin@gmail.com>
2025-01-11 16:21:28 +0100

    Add a ChatPage component for mobile

---
+++ src/pages/ChatPageMobile.jsx
@@ -0,0 +1,27 @@
+import Logout from "../components/Logout";
+import Header from "../components/Header";
+import { useState } from "react";
+import AllChats from "../components/AllChats";
+import { useAuth } from "../utils/useAuth";
+import { Outlet } from "react-router-dom";
+import CreateChat from "../components/CreateChat";
+import styles from "./ChatPage.module.css";
+import { ChatProvider } from "../components/ChatProvider";
+
+function ChatPageMobile() {
+  const { authState } = useAuth();
+  const isMobile = window.innerWidth <= 768;
+
+  return (
+    <>
+      <ChatProvider>
+       
+        <div className={styles.chatPageContainer}>
+          <Outlet />
+        </div>
+      </ChatProvider>
+    </>
+  );
+}
+
+export default ChatPageMobile;
