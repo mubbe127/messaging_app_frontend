f5b4db5ff56c8b12d46835318f0e33503a267570
mubbe127 <mubarek.tajudin@gmail.com>
2025-01-11 16:20:08 +0100

    Set routing logic and rendering logic based on device width and add logic in chat components for device detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+
 import { AuthProvider } from "./utils/useAuth.jsx";
+import AllChats from "./components/AllChats.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
+import ChatPageMobile from "./pages/ChatPageMobile.jsx";
 import Chat from "./components/Chat.jsx";
 import CreateChat from "./components/CreateChat.jsx";
 import CreateMessage from "./components/CreateMessage.jsx";
@@ -11,29 +13,22 @@ import ProfilePage from "./pages/ProfilePage.jsx";
 import "./App.css";
 
 function App() {
+
+  const isMobile = window.innerWidth <= 768;
+  console.log(isMobile)
   return (
     <BrowserRouter>
-      {" "}
-      {/* Ensure BrowserRouter is at the top */}
+    
       <AuthProvider>
-        {" "}
-        {/* AuthProvider is inside BrowserRouter */}
+      
         <Routes>
-          <Route path="/" element={<ChatPage />}>
+          <Route path="/" element={isMobile ? <ChatPageMobile/> : <ChatPage  />}>
+            {isMobile && <Route index element={<AllChats />} />}
             <Route path="new/:memberId?" element={<CreateChat />} />
-            <Route path=":chatId" element={<Chat />} />
-            <Route index element={<CreateChat />} />
+            <Route path="chats/:chatId" element={<Chat />} />
+            {!isMobile && <Route index element={<CreateChat />} />}
           </Route>
-          <Route path="/home" element={<ChatPage />}>
-            <Route index element={<CreateChat />} />
-            <Route path="new/:memberId?" element={<CreateChat />} />
-            <Route path=":chatId" element={<Chat />} />
-          </Route>
-          <Route path="/chats" element={<ChatPage />}>
-            <Route index element={<CreateChat/>} />
-            <Route path="new/:memberId?" element={<CreateChat />} />
-            <Route path=":chatId" element={<Chat />} />
-          </Route>
+     
           <Route path="/users/:userId" element={<ProfilePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
